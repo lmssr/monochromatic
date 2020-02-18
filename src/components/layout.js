3a570f5cd404867229be8e40e88ae5efda10b215
Luc Mosser <luc.mosser86@orange.fr>
2020-02-18 09:50:47 +0100

    netlify continuous deployment test

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,8 +12,11 @@ import { useStaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css";
 import "./layout.css";
 
+import Navbar from './globals/Navbar';
+
 const Layout = ({ children }) => (
   <>
+  <Navbar />
   {children}
   </>
 )
